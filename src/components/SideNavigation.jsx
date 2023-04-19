740284bf48dce073c2721c9f998e13bec2cd9a57
Naomi Cherotich <naomirono1454@gmail.com>
2023-04-19 16:04:56 +0300

    Update code

---
+++ src/components/SideNavigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SideNavigation = () => {
+  return (
+    <div className="fixed h-[100vh] w-16 flex flex-col items-center justify-between bg-gray-200 ">
+  
+</div>
+  );
+};
+
+export default SideNavigation;
